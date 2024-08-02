849b3670c60937e0f1da99d2ccfe5dd7cdff7ae7
Kelvin <kelvintenor987@gmail.com>
2024-08-02 11:42:08 -0300

    Removing the import

--- Main/src/br/com/dio/desafio/dominio/App.java
+++ Main/src/br/com/dio/desafio/dominio/App.java
@@ -1,9 +1,5 @@
 package br.com.dio.desafio.dominio;
 
-import br.com.dio.desafio.dominio.Bootcamp;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Dev;
-import br.com.dio.desafio.dominio.Mentoria;
 import java.time.LocalDate;
 
 public class App {
